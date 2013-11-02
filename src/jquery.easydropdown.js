b761824c525f8d12bf6e4d96ab474fcdb6b20238
Patrick Kunka <patrick.kunka@barrelny.com>
2013-11-02 19:24:07 -0400

    v2.1.3 - fixed scroll bug

--- src/jquery.easydropdown.js
+++ src/jquery.easydropdown.js
@@ -1,6 +1,6 @@
 /*
 * EASYDROPDOWN - A Drop-down Builder for Styleable Inputs and Menus
-* Version: 2.1.2
+* Version: 2.1.3
 * License: Creative Commons Attribution 3.0 Unported - CC BY 3.0
 * http://creativecommons.org/licenses/by/3.0/
 * This software may be used freely on commercial and non-commercial projects with attribution to the author/copyright holder.
@@ -246,7 +246,7 @@
 			});
 			
 			self.$dropDown.on('scroll.easyDropDown',function(e){
-				if(self.$dropDown[0].scrollTop == self.$dropDown[0].scrollHeight - self.maxHeight){
+				if(self.$dropDown[0].scrollTop >= self.$dropDown[0].scrollHeight - self.maxHeight){
 					self.$container.addClass('bottom');
 				} else {
 					self.$container.removeClass('bottom');
