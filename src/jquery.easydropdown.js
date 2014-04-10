da9672ccdda932d963dc1a98ea8d0e076b2f97cf
Timothy Groves <groves@greatwhiteark.com>
2014-04-10 09:43:06 +0200

    Added missing `var`s and semi-colons.
    Removed extra semicolons after `if` statements.

--- src/jquery.easydropdown.js
+++ src/jquery.easydropdown.js
@@ -10,7 +10,7 @@
 
 
 (function($){
-	
+
 	function EasyDropDown(){
 		this.isField = true,
 		this.down = false,
@@ -22,14 +22,14 @@
 		this.nativeTouch = true,
 		this.wrapperClass = 'dropdown',
 		this.onChange = null;
-	};
-	
+	}
+
 	EasyDropDown.prototype = {
 		constructor: EasyDropDown,
 		instances: {},
 		init: function(domNode, settings){
 			var	self = this;
-			
+
 			$.extend(self, settings);
 			self.$select = $(domNode);
 			self.id = domNode.id;
@@ -39,7 +39,7 @@
 			self.$select.removeClass(self.wrapperClass+' dropdown');
 			if(self.$select.is(':disabled')){
 				self.disabled = true;
-			};
+			}
 			if(self.$options.length){
 				self.$options.each(function(i){
 					var $option = $(this);
@@ -47,10 +47,10 @@
 						self.selected = {
 							index: i,
 							title: $option.text()
-						}
+						};
 						self.focusIndex = i;
-					};
-					if($option.hasClass('label') && i == 0){
+					}
+					if($option.hasClass('label') && i === 0){
 						self.hasLabel = true;
 						self.label = $option.text();
 						$option.attr('value','');
@@ -61,24 +61,24 @@
 							value: $option.val(),
 							selected: $option.is(':selected')
 						});
-					};
+					}
 				});
 				if(!self.selected){
 					self.selected = {
 						index: 0,
 						title: self.$options.eq(0).text()
-					}
+					};
 					self.focusIndex = 0;
-				};
+				}
 				self.render();
-			};
+			}
 		},
-	
+
 		render: function(){
 			var	self = this,
 				touchClass = self.isTouch && self.nativeTouch ? ' touch' : '',
 				disabledClass = self.disabled ? ' disabled' : '';
-			
+
 			self.$container = self.$select.wrap('<div class="'+self.wrapperClass+touchClass+disabledClass+'"><span class="old"/></div>').parent().parent();
 			self.$active = $('<span class="selected">'+self.selected.title+'</span>').appendTo(self.$container);
 			self.$carat = $('<span class="carat"/>').appendTo(self.$container);
@@ -91,32 +91,32 @@
 				self.$dropDown.append('<li'+active+'>'+option.title+'</li>');
 			});
 			self.$items = self.$dropDown.find('li');
-			
+
 			if(self.cutOff && self.$items.length > self.cutOff)self.$container.addClass('scrollable');
-			
+
 			self.getMaxHeight();
-	
+
 			if(self.isTouch && self.nativeTouch){
 				self.bindTouchHandlers();
 			} else {
 				self.bindHandlers();
-			};
+			}
 		},
-		
+
 		getMaxHeight: function(){
-			var self = this;
-			
+			var self = this, i;
+
 			self.maxHeight = 0;
-			
+
 			for(i = 0; i < self.$items.length; i++){
 				var $item = self.$items.eq(i);
 				self.maxHeight += $item.outerHeight();
 				if(self.cutOff == i+1){
 					break;
-				};
-			};
+				}
+			}
 		},
-		
+
 		bindTouchHandlers: function(){
 			var	self = this;
 			self.$container.on('click.easyDropDown',function(){
@@ -127,14 +127,14 @@
 					var	$selected = $(this).find('option:selected'),
 						title = $selected.text(),
 						value = $selected.val();
-						
+
 					self.$active.text(title);
 					if(typeof self.onChange === 'function'){
 						self.onChange.call(self.$select[0],{
-							title: title, 
+							title: title,
 							value: value
 						});
-					};
+					}
 				},
 				focus: function(){
 					self.$container.addClass('focus');
@@ -144,7 +144,7 @@
 				}
 			});
 		},
-	
+
 		bindHandlers: function(){
 			var	self = this;
 			self.query = '';
@@ -154,22 +154,22 @@
 						self.open();
 					} else {
 						self.close();
-					};
+					}
 				},
 				'mousemove.easyDropDown': function(){
 					if(self.keyboardMode){
 						self.keyboardMode = false;
-					};
+					}
 				}
 			});
-			
+
 			$('body').on('click.easyDropDown.'+self.id,function(e){
 				var $target = $(e.target),
 					classNames = self.wrapperClass.split(' ').join('.');
 
 				if(!$target.closest('.'+classNames).length && self.down){
 					self.close();
-				};
+				}
 			});
 
 			self.$items.on({
@@ -183,12 +183,12 @@
 						var $t = $(this);
 						$t.addClass('focus').siblings().removeClass('focus');
 						self.focusIndex = $t.index();
-					};
+					}
 				},
 				'mouseout.easyDropDown': function(){
 					if(!self.keyboardMode){
 						$(this).removeClass('focus');
-					};
+					}
 				}
 			});
 
@@ -209,21 +209,21 @@
 						if(key == 38 || key == 40 || key == 32){
 							e.preventDefault();
 							if(key == 38){
-								self.focusIndex--
+								self.focusIndex--;
 								self.focusIndex = self.focusIndex < 0 ? self.$items.length - 1 : self.focusIndex;
 							} else if(key == 40){
-								self.focusIndex++
+								self.focusIndex++;
 								self.focusIndex = self.focusIndex > self.$items.length - 1 ? 0 : self.focusIndex;
-							};
+							}
 							if(!self.down){
 								self.open();
-							};
+							}
 							self.$items.removeClass('focus').eq(self.focusIndex).addClass('focus');
 							if(self.cutOff){
 								self.scrollToView();
-							};
+							}
 							self.query = '';
-						};
+						}
 						if(self.down){
 							if(key == 9 || key == 27){
 								self.close();
@@ -243,9 +243,9 @@
 								self.query += letter;
 								self.search();
 								clearTimeout(self.resetQuery);
-							};
-						};
-					};
+							}
+						}
+					}
 				},
 				'keyup.easyDropDown': function(){
 					self.resetQuery = setTimeout(function(){
@@ -253,26 +253,26 @@
 					},1200);
 				}
 			});
-			
+
 			self.$dropDown.on('scroll.easyDropDown',function(e){
 				if(self.$dropDown[0].scrollTop >= self.$dropDown[0].scrollHeight - self.maxHeight){
 					self.$container.addClass('bottom');
 				} else {
 					self.$container.removeClass('bottom');
-				};
+				}
 			});
-			
+
 			if(self.$form.length){
 				self.$form.on('reset.easyDropDown', function(){
 					var active = self.hasLabel ? self.label : self.options[0].title;
 					self.$active.text(active);
 				});
-			};
+			}
 		},
-		
+
 		unbindHandlers: function(){
 			var self = this;
-			
+
 			self.$container
 				.add(self.$select)
 				.add(self.$items)
@@ -281,7 +281,7 @@
 				.off('.easyDropDown');
 			$('body').off('.'+self.id);
 		},
-		
+
 		open: function(){
 			var self = this,
 				scrollTop = window.scrollY || document.documentElement.scrollTop,
@@ -296,7 +296,7 @@
 			self.$scrollWrapper.css('height',self.maxHeight+'px');
 			self.down = true;
 		},
-		
+
 		close: function(){
 			var self = this;
 			self.$container.removeClass('open');
@@ -305,23 +305,23 @@
 			self.query = '';
 			self.down = false;
 		},
-		
+
 		closeAll: function(){
 			var self = this,
 				instances = Object.getPrototypeOf(self).instances;
 			for(var key in instances){
 				var instance = instances[key];
 				instance.close();
-			};
+			}
 		},
-	
+
 		select: function(index){
-			var self = this;
-			
+			var self = this, i;
+
 			if(typeof index === 'string'){
 				index = self.$select.find('option[value='+index+']').index() - 1;
-			};
-			
+			}
+
 			var	option = self.options[index],
 				selectIndex = self.hasLabel ? index + 1 : index;
 			self.$items.removeClass('active').eq(index).addClass('active');
@@ -333,7 +333,7 @@
 				.prop('selected',true)
 				.parent()
 				.trigger('change');
-				
+
 			self.selected = {
 				index: index,
 				title: option.title
@@ -341,50 +341,50 @@
 			self.focusIndex = i;
 			if(typeof self.onChange === 'function'){
 				self.onChange.call(self.$select[0],{
-					title: option.title, 
+					title: option.title,
 					value: option.value
 				});
-			};
+			}
 		},
-		
+
 		search: function(){
-			var self = this,
+			var self = this, i, title,
 				lock = function(i){
 					self.focusIndex = i;
 					self.$items.removeClass('focus').eq(self.focusIndex).addClass('focus');
-					self.scrollToView();	
+					self.scrollToView();
 				},
 				getTitle = function(i){
 					return self.options[i].title.toUpperCase();
 				};
-				
+
 			for(i = 0; i < self.options.length; i++){
-				var title = getTitle(i);
-				if(title.indexOf(self.query) == 0){
+				title = getTitle(i);
+				if(title.indexOf(self.query) === 0){
 					lock(i);
 					return;
-				};
-			};
-			
+				}
+			}
+
 			for(i = 0; i < self.options.length; i++){
-				var title = getTitle(i);
+				title = getTitle(i);
 				if(title.indexOf(self.query) > -1){
 					lock(i);
 					break;
-				};
-			};
+				}
+			}
 		},
-		
+
 		scrollToView: function(){
 			var self = this;
 			if(self.focusIndex >= self.cutOff){
 				var $focusItem = self.$items.eq(self.focusIndex),
 					scroll = ($focusItem.outerHeight() * (self.focusIndex + 1)) - self.maxHeight;
-			
+
 				self.$dropDown.scrollTop(scroll);
-			};
+			}
 		},
-		
+
 		notInViewport: function(scrollTop){
 			var self = this,
 				range = {
@@ -392,14 +392,14 @@
 					max: scrollTop + (window.innerHeight || document.documentElement.clientHeight)
 				},
 				menuBottom = self.$dropDown.offset().top + self.maxHeight;
-				
+
 			if(menuBottom >= range.min && menuBottom <= range.max){
 				return 0;
 			} else {
 				return (menuBottom - range.max) + 5;
-			};
+			}
 		},
-		
+
 		destroy: function(){
 			var self = this;
 			self.unbindHandlers();
@@ -407,7 +407,7 @@
 			self.$select.unwrap();
 			delete Object.getPrototypeOf(self).instances[self.$select[0].id];
 		},
-		
+
 		disable: function(){
 			var self = this;
 			self.disabled = true;
@@ -415,7 +415,7 @@
 			self.$select.attr('disabled',true);
 			if(!self.down)self.close();
 		},
-		
+
 		enable: function(){
 			var self = this;
 			self.disabled = false;
@@ -423,40 +423,40 @@
 			self.$select.attr('disabled',false);
 		}
 	};
-	
+
 	var instantiate = function(domNode, settings){
 			domNode.id = !domNode.id ? 'EasyDropDown'+rand() : domNode.id;
 			var instance = new EasyDropDown();
 			if(!instance.instances[domNode.id]){
 				instance.instances[domNode.id] = instance;
 				instance.init(domNode, settings);
-			};
+			}
 		},
 		rand = function(){
 			return ('00000'+(Math.random()*16777216<<0).toString(16)).substr(-6).toUpperCase();
 		};
-	
+
 	$.fn.easyDropDown = function(){
 		var args = arguments,
 			dataReturn = [],
 			eachReturn;
-			
+
 		eachReturn = this.each(function(){
 			if(args && typeof args[0] === 'string'){
 				var data = EasyDropDown.prototype.instances[this.id][args[0]](args[1], args[2]);
 				if(data)dataReturn.push(data);
 			} else {
 				instantiate(this, args[0]);
-			};
+			}
 		});
-		
+
 		if(dataReturn.length){
 			return dataReturn.length > 1 ? dataReturn : dataReturn[0];
 		} else {
 			return eachReturn;
-		};
+		}
 	};
-	
+
 	$(function(){
 		if(typeof Object.getPrototypeOf !== 'function'){
 			if(typeof 'test'.__proto__ === 'object'){
@@ -467,12 +467,12 @@
 				Object.getPrototypeOf = function(object){
 					return object.constructor.prototype;
 				};
-			};
-		};
-		
+			}
+		}
+
 		$('select.dropdown').each(function(){
-			var json = $(this).attr('data-settings');
-				settings = json ? $.parseJSON(json) : {}; 
+			var json = $(this).attr('data-settings'),
+				settings = json ? $.parseJSON(json) : {};
 			instantiate(this, settings);
 		});
 	});
