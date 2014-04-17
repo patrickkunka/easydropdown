484ecae649c40042c2266087823c16bb85d235d0
Agustin Villalba <avillalbacasas@gmail.com>
2014-04-16 22:46:07 -0400

    Changed logic in method instantiate to reset instances for AJAX requests

--- src/jquery.easydropdown.js
+++ src/jquery.easydropdown.js
@@ -427,10 +427,11 @@
 	var instantiate = function(domNode, settings){
 			domNode.id = !domNode.id ? 'EasyDropDown'+rand() : domNode.id;
 			var instance = new EasyDropDown();
-			if(!instance.instances[domNode.id]){
-				instance.instances[domNode.id] = instance;
-				instance.init(domNode, settings);
-			};
+			if(instance.instances[domNode.id]) {
+        instance.instances[domNode.id] = null;
+      };
+      instance.instances[domNode.id] = instance;
+      instance.init(domNode, settings);
 		},
 		rand = function(){
 			return ('00000'+(Math.random()*16777216<<0).toString(16)).substr(-6).toUpperCase();
